21b6db4ed3253203c1bf6de4efce76ce6fa9abf6
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-04-19 15:53:37 +0800

    cmake

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -196,4 +196,22 @@ return {
       },
     },
   },
+  {
+    "Civitasv/cmake-tools.nvim",
+    opts = {
+      cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
+      cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
+      cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
+      cmake_build_directory = function()
+        return "cmake-build-${variant:buildType}"
+      end, -- this is used to specify generate directory for cmake, allows macro expansion, can be a string or a function returning the string, relative to cwd.
+      cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
+      cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
+      cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
+      cmake_variants_message = {
+        short = { show = true }, -- whether to show short message
+        long = { show = true, max_length = 40 }, -- whether to show long message
+      },
+    },
+  },
 }
