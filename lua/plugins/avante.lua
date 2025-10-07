0a54fd871579a441a1b76d68701b276560101fc6
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-10-07 21:07:48 +0800

    reconfig avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,99 +1,77 @@
 return {
   {
     "yetone/avante.nvim",
-    lazy = true,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
     event = "VeryLazy",
-    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
-
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
-      provider = "copilot",
-      auto_suggestions_provider = "copilot",
-      --copilot = {
-      --  model = "claude-3.5-sonnet",
-      --  temperature = 0,
-      --  --max_tokens = 8192,
-      --},
-      hints = { enabled = false },
-      file_selector = {
-        provider = "snacks",
-        provider_opts = {},
-      },
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "avante.md",
+      -- for example
+      provider = "claude",
       providers = {
-        copilot = {
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-sonnet-4-20250514",
+          timeout = 30000, -- Timeout in milliseconds
           extra_request_body = {
-            max_tokens = 8192,
-            temperature = 0,
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
+        moonshot = {
+          endpoint = "https://api.moonshot.ai/v1",
+          model = "kimi-k2-0711-preview",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 32768,
           },
         },
       },
     },
-
     dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = function(_, ft)
-          vim.list_extend(ft, { "Avante" })
-        end,
-      },
-      {
-        "folke/which-key.nvim",
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
         opts = {
-          spec = {
-            { "<leader>a", group = "ai" },
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
           },
         },
       },
-    },
-  },
-
-  {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    opts = {
-      input = { enabled = false },
-      select = { enabled = false },
-    },
-  },
-
-  {
-    "saghen/blink.compat",
-    lazy = true,
-    opts = {},
-    config = function()
-      -- monkeypatch cmp.ConfirmBehavior for Avante
-      require("cmp").ConfirmBehavior = {
-        Insert = "insert",
-        Replace = "replace",
-      }
-    end,
-  },
-
-  {
-    "saghen/blink.cmp",
-    lazy = true,
-    opts = {
-      sources = {
-        default = { "avante_commands", "avante_mentions", "avante_files" },
-        providers = {
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 90, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_files = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 100, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
         },
+        ft = { "markdown", "Avante" },
       },
     },
   },
