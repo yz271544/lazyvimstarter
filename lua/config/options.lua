741a06d34dd1beb71c381eb91419b55d18701c29
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2026-06-02 22:22:33 +0800

    fix: move leap.nvim plugin from options.lua to plugins/
    The options.lua file should only contain vim.opt settings, not plugin
    specs. Moved leap.nvim config to lua/plugins/leap.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,24 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-#vim.opt.clipboard = "unnamedplus"
-return {
-  {
-    "ggandor/leap.nvim",
-    enabled = true,
-    keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
-    },
-    config = function(_, opts)
-      local leap = require("leap")
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-    end,
-  },
-}
+vim.opt.clipboard = "unnamedplus"
